36d82c62a126c22a697cd47b9e051fe086c42dc3
DIANACF <djcarranza@uniboyaca.edu.co>
2024-09-17 20:02:52 -0500

    Inicio del diseño del formulario de la creación de una materia

--- promedioSemestral/src/app/materias/materias.page.ts
+++ promedioSemestral/src/app/materias/materias.page.ts
@@ -1,14 +1,18 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar,
+  IonCard,IonCardContent, IonCardTitle, IonInput, IonButton
+ } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-materias',
   templateUrl: './materias.page.html',
   styleUrls: ['./materias.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule]
+  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
+    IonCard,IonCardContent,IonCardTitle, IonInput, IonButton
+  ]
 })
 export class MateriasPage implements OnInit {
 
