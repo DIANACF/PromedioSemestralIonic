39ca03cc28a6ed2b6d4c15e1aaf45e6905b3b0f0
DIANACF <djcarranza@uniboyaca.edu.co>
2024-09-22 12:30:10 -0500

    creación del botón menú y sus opciones en eñ toolbar

--- promedioSemestral/src/app/materias/materias.page.ts
+++ promedioSemestral/src/app/materias/materias.page.ts
@@ -2,8 +2,11 @@ import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 import { IonContent, IonHeader, IonTitle, IonToolbar,
-  IonCard,IonCardContent, IonCardTitle, IonInput, IonButton
+  IonCard,IonCardContent, IonCardTitle, IonInput, IonButton,
+  IonButtons, IonMenuButton,IonMenu, IonList,IonItem, IonLabel, IonIcon,
+  IonRouterLink
  } from '@ionic/angular/standalone';
+//import { IonMenu } from '@ionic/angular';
 
 @Component({
   selector: 'app-materias',
@@ -11,7 +14,8 @@ import { IonContent, IonHeader, IonTitle, IonToolbar,
   styleUrls: ['./materias.page.scss'],
   standalone: true,
   imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule,
-    IonCard,IonCardContent,IonCardTitle, IonInput, IonButton
+    IonCard,IonCardContent,IonCardTitle, IonInput, IonButton, IonButtons,
+    IonMenuButton, IonMenu, IonList,IonItem, IonLabel,IonIcon,IonRouterLink
   ]
 })
 export class MateriasPage implements OnInit {
